7faa48ce0fb2effe972171133f8381f841a70c20
Kelechi Nwauwa <nwauwa.kelechi.e@gmail.com>
2021-09-16 11:47:02 +0100

    first 'successful' commit

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="flex flex-row bg-gray-400 h-1/6 w-full justify-space-between">
+    <div>
+      <a href="#">Menu</a>
+    </div>
+    <div>
+      <a href="#">Code like Ada</a>
+    </div>
+    <div>
+      <a href="#">Contact an Administrator</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
